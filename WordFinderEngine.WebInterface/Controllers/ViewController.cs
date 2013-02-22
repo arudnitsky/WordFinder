7dbc3ffe3edaf9ef008654a0d1fb35151940878f
J.Kling <J.Kling@techsmith.com>
2013-02-22 10:59:56 -0500

    Add "fancy" (not really) UI with javascript

---
+++ WordFinderEngine.WebInterface/Controllers/ViewController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WordFinderEngine.WebInterface.Controllers
+{
+    public class ViewController : Controller
+    {
+
+       public ActionResult Index()
+       {
+           return View();
+       }
+
+       public PartialViewResult GetWords( string letters )
+       {
+          if( letters == null ) return PartialView( new List<string>() );
+
+          var engine = new Engine();
+          var words = engine.FindWords( new List<char>( letters ) );
+          words.Sort( new LengthComparer() );
+          return PartialView( words );
+       }
+
+    }
+
+    public class LengthComparer : IComparer<string>
+    {
+       public int Compare( string x, string y )
+       {
+          // Handle nulls
+          if ( x == null && y == null )
+             return 0;
+          if ( x == null )
+             return -1;
+          if ( y == null )
+             return 1;
+
+          // Handle different lengths
+          if ( x.Length > y.Length )
+             return 1;
+
+          if ( y.Length > x.Length )
+             return -1;
+
+          // Handle same length
+          return x.CompareTo( y );
+       }
+    }
+}
---
+++ WordFinderEngine.WebInterface/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle( "WordFinderEngine.WebInterface" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct( "WordFinderEngine.WebInterface" )]
+[assembly: AssemblyCopyright( "Copyright ©  2013" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid( "15539351-b46e-42b0-a3a4-9eaea76a80e9" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
